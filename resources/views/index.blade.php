3306c2bfc2a86dd22d108d18e0fd4e947316764e
Bishoy Gerges <bishoy_gerges@outlook.com>
2023-10-24 17:31:37 -0400

    Class and obj's in web to be changed later

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,7 +1,10 @@
-Hello I'm a blade template
-
-@isset($name, $age, $work)
-My name is {{ $name }}
-my age is {{ $age }}
-I work at  {{ $work }}
-@endisset
+<h1>The list of Tasks</h1>
+<div>
+   {{-- @if (count($tasks)) --}}
+    @forelse ($tasks as $task)
+        <div> <a href="{{ route('tasks.show', ['id'=> $task ->id]) }}"> {{ $task->title}}</a></div>
+        @empty
+           <div> There are no tasks</div> 
+    @endforelse
+   {{-- @endif --}}
+</div>
