061feb2aadeaf883b7929c098df38a3b045b79f9
Bishoy Gerges <bishoy_gerges@outlook.com>
2023-11-04 18:01:11 -0400

    Adding css to single page task

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -3,34 +3,36 @@
 @section('title', $task->title)
 
 @section('content')
-<p> {{ $task->description }}</p>
+<div class="mb-4">
+    <a href="{{ route('tasks.index') }}"
+      class="link"><- Back to list</a>
+</div>
+<p class="mb-4 text-slate-700"> {{ $task->description }}</p>
 
 @if ($task->long_description)
-    <p> {{ $task->long_description }}</p>
+    <p class="mb-4 text-slate-700"> {{ $task->long_description }}</p>
 @endif
 
-<p> {{ $task->created_at }}</p>
-<p> {{ $task->updated_at }}</p>
-<div class="mb4">
+<p class="mb-4 text-sm text-slate-500">Created {{ $task->created_at->diffForHumans() }} . Updated {{ $task->updated_at->diffForHumans() }}</p>
+<div class="mb-4">
     @if ($task->completed)
-        Completed
+        <span class="font-medium text-green-500">Completed</span>
         @else
-        Not completed
+        <span class="font-medium text-red-500">Not completed</span>
     @endif
 </div>
-<div class="mb-4 mt-4">
-    <a class="btn" href="{{route('tasks.edit', ['task'=> $task]) }}">Edit</a>
-</div>
-<div class="mb-4">
+<div class="flex gap-2">
+    <a class="btn" 
+    href="{{route('tasks.edit', ['task'=> $task]) }}">Edit</a>
     <form action="{{ route('tasks.toggle', ['task' =>$task]) }}" method="POST">
         @csrf
         @method('PUT')
         <button type="submit" class="btn">Mark as {{ $task->completed ? 'not completed' : 'completed' }} </button>
     </form>
-</div class="mb-4">
-<form action="{{ route('tasks.destroy', ['task' => $task->id]) }}" method="POST">
-    @csrf
-    @method('DELETE')
-    <button type="submit" class="btn">Delete</button>
-</form>
+    <form action="{{ route('tasks.destroy', ['task' => $task->id]) }}" method="POST">
+        @csrf
+        @method('DELETE')
+        <button type="submit" class="btn">Delete</button>
+    </form>
+</div>
 @endsection
