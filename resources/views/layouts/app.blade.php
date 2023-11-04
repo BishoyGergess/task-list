a6346c3ff692b0d0b13877040aa181eac10be61c
Bishoy Gerges <bishoy_gerges@outlook.com>
2023-11-04 01:50:10 -0400

    Started to work with Tailwind Css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,13 +2,15 @@
 <html>
     <head>
         <title> Laravel 10 Task list APP</title>
+        <script src="https://cdn.tailwindcss.com"></script>
     </head>
-    <body>
+    <body class="container mx-auto mt-10 mb-10 max-w-lg">
         
+
+        <h1 class="mb-4 text-2xl">@yield('title')</h1>
         @if (session()->has('success'))
-         <div class="success-message">{{ session('success') }}</div>
-        @endif
-        <h1>@yield('title')</h1>
+        <div class="success-message">{{ session('success') }}</div>
+       @endif
         <div>@yield('content')</div>
     </body>
 </html>
